fb6563f247358c04f863c68f4eb758eec7e267c7
Alexandre Mota <alexandremot@gmail.com>
2023-04-02 17:11:18 -0300

    melhoria no metodo get e tratamento de erros; application.properties para application.yml

--- src/main/java/com/example/devdojospringboot/service/AnimeService.java
+++ src/main/java/com/example/devdojospringboot/service/AnimeService.java
@@ -2,14 +2,26 @@ package com.example.devdojospringboot.service;
 
 import com.example.devdojospringboot.domain.Anime;
 import com.example.devdojospringboot.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService implements AnimeRepository {
 
+    private List<Anime> animes = List.of(new Anime(1L, "Attack on Titan"), new  Anime(2L, "Naruto"));
+
     public List<Anime> listAll(){
-        return List.of(new Anime(1L, "Attack on Titan"), new  Anime(2L, "Naruto"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        Anime result = animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found!"));
+        return result;
     }
 }
