a499b08156f4c913103452f84e372a277848ddf1
Alexandre Mota <alexandremot@gmail.com>
2023-04-02 17:32:41 -0300

    metodo post

--- src/main/java/com/example/devdojospringboot/service/AnimeService.java
+++ src/main/java/com/example/devdojospringboot/service/AnimeService.java
@@ -6,12 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService implements AnimeRepository {
 
-    private List<Anime> animes = List.of(new Anime(1L, "Attack on Titan"), new  Anime(2L, "Naruto"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Attack on Titan"), new  Anime(2L, "Naruto")));
+    }
 
     public List<Anime> listAll(){
         return animes;
@@ -23,5 +29,11 @@ public class AnimeService implements AnimeRepository {
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found!"));
         return result;
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+        return anime;
     }
 }
